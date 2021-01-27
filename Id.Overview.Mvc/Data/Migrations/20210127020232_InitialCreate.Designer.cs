176ef4c73e205b992e92f006223997eea323ddd5
luccanog <luccathomaz@gmail.com>
2021-01-26 23:12:13 -0300

    fixing auto generated EF migrations

--- Id.Overview.Mvc/Data/Migrations/20210127020232_InitialCreate.Designer.cs
+++ Id.Overview.Mvc/Data/Migrations/20210127020232_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Id.Overview.Mvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Id.Overview.Mvc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210127020232_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
